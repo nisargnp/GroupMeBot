9862a8f5b79dec7db865e6d0f4d92c952735f159
Daniel Choi <danielchoi42@gmail.com>
2017-03-15 00:39:54 -0400

    fixed minor syntax error

--- app.js
+++ app.js
@@ -212,11 +212,10 @@ app.post('/', function(req,res) {
 			if (s[0] == '!twitch' && s.length == 2) {
 				getTwitchStatus(s[1]);
 			}
-		} else if (/^!youtube (.*)/.test(body))
+		} else if (/^!youtube (.*)/.test(body)) {
 			var s = RegExp.$1;
 			searchYouTube(s);
 		}
-
 	}
 
 	res.send("Success");
